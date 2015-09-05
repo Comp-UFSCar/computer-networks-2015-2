3d333e26ee9cbe1675f6ac19d8b72507f53dbaa4
thamenato <thamenato@gmail.com>
2015-09-05 15:47:50 -0300

    First version of a working backend/daemon
    It works if used locally but some tests must be applied and a multithread version will be implement soon. Also there isn't any application inside the daemon for now, only communication check for now.

--- projeto1/backend.py
+++ projeto1/backend.py
@@ -1,1 +1,103 @@
 __author__ = 'Thales Menato and Thiago Nogueira'
+
+import socket
+import sys
+
+# Protocol HEADER - ASCII in a single String:
+#
+#   ["REQUEST"|"RESPONSE"][1-4][<parameters>]
+#
+# where:
+#   1 - "ps"
+#   2 - "df"
+#   3 - "finger"
+#   4 - "uptime"
+# and <parameters> can only be used in a "REQUEST" message, also, the daemon must parse
+# and verify malicious inputs like "|", ";", ">", so they're not executed
+
+# Class Protocol
+class Protocol:
+    # Standardization of communication protocol between backend and daemon
+    def __init__(self):
+        self.type = None            # REQUEST or RESPONSE
+        self.command = None         # if REQUEST, which command will be sent
+        self.parameters = None      # parameters for command
+        self.response = None        # if RESPONSE, the response from executed command
+
+    # Create a REQUEST header and content
+    def createRequest(self, command, parameters = None):
+        self.type = "REQUEST"
+        self.command = command
+        self.parameters = parameters
+        if parameters is None:
+            return self.type + " " + str(self.command)
+        else:
+            return self.type + " " + str(self.command) + " " + str(self.parameters)
+
+    # Create a RESPONSE header and content
+    def createResponse(self, command, response):
+        self.type = "RESPONSE"
+        self.command = command
+        self.response = response
+        return self.type + " " + str(self.command) + " " + str(self.response)
+
+class BackEnd:
+
+    HOSTS = {}  # all the ip:port from machines with daemon
+    SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Socket UDP
+
+    @staticmethod
+    def addHost(host):
+        BackEnd.HOSTS[host] = None # insert a tuple with ip:port
+        print "Host " + str(host) + " added."
+
+    @staticmethod
+    def mountPackages(host, protocol):
+        print "\nMounting packages..."
+
+        if len(host) is not len(protocol):
+            print "\terror: different length of hosts and protocol, both should be lists."
+
+        else:
+            for i in range(0, len(host)):
+                BackEnd.HOSTS[host[i]] = protocol[i]
+                print "\t{}\t:\t{}".format(host[i], protocol[i])
+        print "...finished.\n"
+
+    @staticmethod
+    def sendAll():
+        #TODO: testar com N daemons na rede
+        print "\nSending to daemons..."
+        for host in BackEnd.HOSTS:
+            BackEnd.SOCK.sendto(str(BackEnd.HOSTS[host]) + "\n", host)
+            print "\tSent from {}:\t{}".format(host,BackEnd.HOSTS[host])
+        print "...finished.\n"
+
+    @staticmethod
+    def receiveAll():
+        #TODO: testar metodo para N daemons na rede - como ele recebe os dados
+        print "\nReceiving data from daemons..."
+        received = BackEnd.SOCK.recv(1024)
+        print "\tReceived:\t{}".format(received)
+        print "...finished.\n"
+
+
+if __name__ == '__main__':
+
+    # Host list
+    hosts = [('127.0.0.1', 9999)]
+
+    # Add hosts to BackEnd
+    for h in hosts:
+        BackEnd.addHost(h)
+
+    # Protocol list -- message to be sent
+    p = []
+    p.append(Protocol().createRequest(1))
+
+    # Mount packages, assumes hosts and packages have same order respectively
+    BackEnd.mountPackages(hosts,p)
+    # Send to 'key' host the 'value' protocol inside HOSTS dictionary
+    BackEnd.sendAll()
+    # Receive everything from daemon
+    BackEnd.receiveAll()
--- projeto1/daemon.py
+++ projeto1/daemon.py
@@ -1,1 +1,21 @@
 __author__ = 'Thales Menato and Thiago Nogueira'
+
+# Client script
+
+#from backend import Protocol
+import SocketServer
+
+class MyUDPHandler(SocketServer.BaseRequestHandler):
+    #TODO: verificar se o comando do request nao eh malicioso
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        print"{} wrote:".format(self.client_address[0])
+        print data
+        socket.sendto(data.lower(), self.client_address)
+
+if __name__ == '__main__':
+    HOST, PORT = "localhost", 9999
+    server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
+    print "Daemon started -- " + str(HOST) +":"+ str(PORT)
+    server.serve_forever()
