96fe8fe3b85c05e813774bbeca96c04ec55118f7
thamenato <thamenato@gmail.com>
2015-09-11 20:48:17 -0300

    Functional version with multiples daemons
    This version works with multiple daemons (N machines). The communication is done host by host, sequentially.
    Multithreaded will be implemented later.

--- projeto1/backend.py
+++ projeto1/backend.py
@@ -43,61 +43,90 @@ class Protocol:
 
 class BackEnd:
 
-    HOSTS = {}  # all the ip:port from machines with daemon
+    HOSTS = {}  # Key is tuple (ip, port) and Value is Protocol that will be sent
     SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Socket UDP
 
+    # Add a Host to the HOSTS dictionary
     @staticmethod
     def addHost(host):
         BackEnd.HOSTS[host] = None # insert a tuple with ip:port
-        print "Host " + str(host) + " added."
+        print "\tHost " + str(host) + " added."
 
+    # Add a Package to determined Host
     @staticmethod
-    def mountPackages(host, protocol):
-        print "\nMounting packages..."
+    def addPackage(host, protocol):
+        if BackEnd.HOSTS.has_key(host) is False:
+            print "\tError on trying to add package to {} - host doesn't exist.".format(host)
+        else:
+            BackEnd.HOSTS[host] = protocol
 
-        if len(host) is not len(protocol):
-            print "\terror: different length of hosts and protocol, both should be lists."
+    # From a list of Hosts and list of Protocol, attribute each protocol to each host, respectively
+    @staticmethod
+    def mountPackages(hosts_list, protocol_list):
+        print "\n\tMounting packages..."
+
+        if len(hosts_list) is not len(protocol_list):
+            print "\t\terror: different length of hosts and protocol, both should be lists."
 
         else:
-            for i in range(0, len(host)):
-                BackEnd.HOSTS[host[i]] = protocol[i]
-                print "\t{}\t:\t{}".format(host[i], protocol[i])
-        print "...finished.\n"
+            for i in range(0, len(hosts_list)):
+                BackEnd.HOSTS[hosts_list[i]] = protocol_list[i]
+                print "\t\t{}\t:\t{}".format(hosts_list[i], protocol_list[i])
+        print "...finished."
 
+    # Send all packages to all hosts
     @staticmethod
     def sendAll():
         #TODO: testar com N daemons na rede
-        print "\nSending to daemons..."
+        data = []
+        print "\tSending to daemons..."
         for host in BackEnd.HOSTS:
-            BackEnd.SOCK.sendto(str(BackEnd.HOSTS[host]) + "\n", host)
-            print "\tSent from {}:\t{}".format(host,BackEnd.HOSTS[host])
-        print "...finished.\n"
+            # Sending to HOST
+            send_msg = str(BackEnd.HOSTS[host])
+            BackEnd.SOCK.sendto(send_msg, host)
 
-    @staticmethod
-    def receiveAll():
-        #TODO: testar metodo para N daemons na rede - como ele recebe os dados
-        print "\nReceiving data from daemons..."
-        received = BackEnd.SOCK.recv(1024)
-        print "\tReceived:\t{}".format(received)
-        print "...finished.\n"
+            # Receiving from HOST
+            #TODO: parser na msg de volta, separando o HEADER do Protocolo com o retorno do comando
+            BackEnd.HOSTS[host] = recv_msg = BackEnd.SOCK.recv(1024)
+            print "\t\tSent to {} | msg: {}\n\t\treceived:\n{}\n".format(host,send_msg,recv_msg)
+
+        print "...finished."
 
 
 if __name__ == '__main__':
-
+    print "\nBackend test started..."
     # Host list
-    hosts = [('127.0.0.1', 9999)]
+    hosts = [('192.168.1.101', 9999)]
 
     # Add hosts to BackEnd
     for h in hosts:
         BackEnd.addHost(h)
 
-    # Protocol list -- message to be sent
-    p = []
-    p.append(Protocol().createRequest(1))
+    # Trying to add package to a non existing host
+    print "\nTrying to add a package to a nonexistent Host..."
+    BackEnd.addPackage(('192.168.1.2', 9999), Protocol().createRequest(1))
+
+    print "\nCreating request for 1 - ps ..."
+    # Adding package to first host
+    BackEnd.addPackage(hosts[0], Protocol().createRequest(1))
 
     # Mount packages, assumes hosts and packages have same order respectively
-    BackEnd.mountPackages(hosts,p)
+    #BackEnd.mountPackages(hosts,p)
+
     # Send to 'key' host the 'value' protocol inside HOSTS dictionary
     BackEnd.sendAll()
-    # Receive everything from daemon
-    BackEnd.receiveAll()
+
+    print "\nCreating request for 2 - df ..."
+
+    BackEnd.addPackage(hosts[0], Protocol().createRequest(2))
+    BackEnd.sendAll()
+
+    print "\nCreating request for 3 - finger ..."
+
+    BackEnd.addPackage(hosts[0], Protocol().createRequest(3))
+    BackEnd.sendAll()
+
+    print "\nCreating request for 4 - uptime ..."
+
+    BackEnd.addPackage(hosts[0], Protocol().createRequest(4))
+    BackEnd.sendAll()
--- projeto1/daemon.py
+++ projeto1/daemon.py
@@ -1,21 +1,105 @@
 __author__ = 'Thales Menato and Thiago Nogueira'
 
-# Client script
+# Daemon Script
+#
+# This module works as a daemon on each host machine that will receive commands from the backend.py
+# It uses the Protocol defined in backend (check that script for more information)
+#
 
-#from backend import Protocol
 import SocketServer
+import os
+import socket
+import sys
+if os.name != "nt":
+    import fcntl
+    import struct
+from backend import Protocol
+import commands
+
+# module for getting the lan ip address of the computer
+class IPGetter:
+    # This solution was found at - http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+    # and it was suggested by user 'smerlin' ( http://stackoverflow.com/users/231717/smerlin )
+    @staticmethod
+    def get_interface_ip(ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+                                ifname[:15]))[20:24])
+
+    @staticmethod
+    def get_lan_ip():
+        ip = socket.gethostbyname(socket.gethostname())
+        if ip.startswith("127.") and os.name != "nt":
+            interfaces = ["eth0","eth1","eth2","wlan0","wlan1","wifi0","ath0","ath1","ppp0"]
+            for ifname in interfaces:
+                try:
+                    ip = IPGetter.get_interface_ip(ifname)
+                    break
+                except IOError:
+                    pass
+        return ip
+
 
 class MyUDPHandler(SocketServer.BaseRequestHandler):
-    #TODO: verificar se o comando do request nao eh malicioso
+    # where:
+#   1 - "ps"
+#   2 - "df"
+#   3 - "finger"
+#   4 - "uptime"
+
+    def response_ps(self, socket, command, client_address):
+        socket.sendto(Protocol().createResponse(command, str(commands.getoutput("ps"))), client_address)
+
+    def response_df(self, socket, command, client_address):
+        socket.sendto(Protocol().createResponse(command, str(commands.getoutput("df"))), client_address)
+
+    def response_finger(self, socket, command, client_address):
+        socket.sendto(Protocol().createResponse(command, str(commands.getoutput("finger"))), client_address)
+
+    def response_uptime(self, socket, command, client_address):
+        socket.sendto(Protocol().createResponse(command, str(commands.getoutput("uptime"))), client_address)
+
+    options = {
+        "1" : response_ps,
+        "2" : response_df,
+        "3" : response_finger,
+        "4" : response_uptime,
+    }
+
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        print"{} wrote:".format(self.client_address[0])
-        print data
-        socket.sendto(data.lower(), self.client_address)
+        print "Received {} from {}".format(data, self.client_address[0])
+
+        #TODO: verificar se o comando do request nao eh malicioso
+        data = str(data).split()
+
+        if str(data[0]) in ["REQUEST"]:
+            self.options.get(data[1])(self, socket, data[1], self.client_address)
+
+        else:
+            print "Not a REQUEST."
+            socket.sendto(Protocol().createResponse("ERROR","NOT A REQUEST"), self.client_address)
 
 if __name__ == '__main__':
-    HOST, PORT = "localhost", 9999
+    print "Starting Daemon..."
+    # Port that will be used -- change this if necessary
+    PORT = 9999
+    # If no argument was used
+    if len(sys.argv) is 1:
+        HOST = IPGetter.get_lan_ip()
+        print "No argument found, using {}:{}".format(HOST,PORT)
+    else:
+        # verify if argument is a valid IP
+        try:
+            socket.inet_aton(sys.argv[1])
+            HOST = str(sys.argv[1])
+            print "Argument found, using {}:{}".format(HOST, PORT)
+        except socket.error:
+            print "Invalid argument - wrong ip: {}".format(sys.argv[1])
+            exit(1)
+
+    # Starts server
     server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
-    print "Daemon started -- " + str(HOST) +":"+ str(PORT)
+    print "...daemon initialized."
     server.serve_forever()
