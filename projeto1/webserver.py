e326b9d5e1a839c1be9650a54e70537ca094788e
thamenato <thamenato@hotmail.com>
2015-09-14 14:53:31 -0300

    Use of index.html and linux environment
    Changes suggested by @ThiagoTioGuedes - use a html page and the header from webserver.py to work with linux os.

--- projeto1/webserver.py
+++ projeto1/webserver.py
@@ -1,5 +1,4 @@
-#!C:\Python27\python.exe -u
-#!/usr/bin/env python
+#!/usr/bin/python
 
 __author__ = "Thales Menato and Thiago Nogueira"
 
@@ -12,34 +11,10 @@ import cgitb; cgitb.enable()
 print "Content-type : text/html"
 print
 
-# print """
-# <html>
-# <head>
-#     <title>Sample CGI Example</title>
-# </head>
-#
-# <body>
-#     <h3>Sample CGI Example</h3>
-# """
-
-print
-with open('html\header.html') as f:
-    print f.read()
-
 form = cgi.FieldStorage()
 message = form.getvalue("message", "(no message)")
 
 print
-with open('html\/form.html') as f:
+with open("index.html") as f:
     print f.read() % cgi.escape(message)
 
-# print """
-#     <p>Previous message: %s</p>
-#     <p>form
-#
-#     <form method="post" action="webserver.py">
-#         <p>message: <input type="text" name="message"/></p>
-#     </form>
-# </body>
-# </html>
-# """ % cgi.escape(message)
