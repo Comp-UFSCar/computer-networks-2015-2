d931dcc656b1bddac332e895ab9cb37c079aba22
Camilo Moreira <camilomoreira91@gmail.com>
2015-11-16 10:52:54 -0200

    Remove class structure from Checksum to be used as module. Rename local variables.

--- projeto2/src/toolbox/checksum.py
+++ projeto2/src/toolbox/checksum.py
@@ -10,86 +10,82 @@ data, they will not be detected.
 
 How to use:
     To find the checksum of a bunch of binary data
-    checksum = Checksum.compute(data)
+    checksum = compute(data)
 
     To verify if a bunch of binary data has a specific checksum
-    Checksum.verify(data,checksum)
+    verify(data,checksum)
 
 """
 
-class Checksum:
+__MAX_INT = 2 ** 16 - 1
+""" int: Maximum 16-bits integer number for modulo operation (via bitwise operation &)."""
 
-    __MAX_INT = 2 ** 16 - 1
-    """ int: Maximum 16-bits integer number for modulo operation (via bitwise operation &)."""
 
-    @staticmethod
-    def __compute(data):
-        """Compute the checksum value of data
+def __compute(_data):
+    """Compute the checksum value of data
 
-        Args:
-        data (str): batch of data whose checksum value will be computed
+    Args:
+        _data (str): batch of data whose checksum value will be computed
 
-        Returns:
+    Returns:
         checksum (int): checksum value
 
-        """
-        size = len(data)
-        remainder = size % 2
-        payload = data
-        # If data has an odd number of bytes, one empty byte is added to it
-        if remainder == 1:
-            payload += '\0'
-        # Breaks payload in individual bytes to be processed
-        payload = list(payload)
-        # Byte array is traversed every two bytes, then half of its size
-        size = (size + remainder) / 2
-        checksum = 0
+    """
+    _size = len(_data)
+    _remainder = _size % 2
+    _payload = _data
+    # If data has an odd number of bytes, one empty byte is added to it
+    if _remainder == 1:
+        _payload += '\0'
+    # Breaks payload in individual bytes to be processed
+    _payload = list(_payload)
+    # Byte array is traversed every two bytes, then half of its size
+    _size = (_size + _remainder) / 2
+    _checksum = 0
 
-        while size > 0:
-            # Byte array is traversed backwards.
-            size -= 1
-            # Number from 0 to 255 shifted 8 times to the left
-            m = ord(payload[2 * size]) << 8
-            # Number from 0 to 255
-            n = ord(payload[2 * size + 1])
-            # Checksum is an accumulated sum of two adjacent bytes
-            # If the sum is a number greater than 2 bytes, it is rounded to 2 bytes by __MAX_INT
-            checksum = (checksum + (m | n)) & Checksum.__MAX_INT
-        return checksum
+    while _size > 0:
+        # Byte array is traversed backwards.
+        _size -= 1
+        # Number from 0 to 255 shifted 8 times to the left
+        _byte_1 = ord(_payload[2 * _size]) << 8
+        # Number from 0 to 255
+        _byte_0 = ord(_payload[2 * _size + 1])
+        # Checksum is an accumulated sum of two adjacent bytes
+        # If the sum is a number greater than 2 bytes, it is rounded to 2 bytes by __MAX_INT
+        _checksum = (_checksum + (_byte_1 | _byte_0)) & __MAX_INT
+    return _checksum
 
-    @staticmethod
-    def compute(data):
-        """Compute the checksum value of data
 
-        Args:
-        data (str): batch of data whose checksum value will be computed
+def compute(_data):
+    """Compute the checksum value of data
 
-        Returns:
+    Args:
+        _data (str): batch of data whose checksum value will be computed
+
+    Returns:
         checksum (int): 2's complement of checksum value
 
-        """
-        c = Checksum.__compute(data)
+    """
+    _c = __compute(_data)
 
-        # The 2's complement of a binary number X is NOT(X) + 1
-        return ((~c) + 1) & Checksum.__MAX_INT
+    # The 2's complement of a binary number X is NOT(X) + 1
+    return ((~_c) + 1) & __MAX_INT
 
-    @staticmethod
-    def verify(data, checksum):
-        """Verify if a pair of data and checksum value is valid
 
-        Args:
-        data (str): batch of data whose checksum value will be computed
-        checksum (int): checksum value
+def verify(_data, _checksum):
+    """
+    Verify if a pair of data and checksum value is valid
 
-        Returns:
-        True    if it is a valid pair of data and checksum
-        False   otherwise"""
+    Args:
+        _data (str): batch of data whose checksum value will be computed
+        _checksum (int): checksum value
 
-        d = raw_input()
-        c = Checksum.compute(d)
-        print Checksum.verify(d, c+1)
-        print Checksum.verify(d, c)
-        c = Checksum.__compute(data)
+    Returns:
+        True if it is a valid pair of data and checksum,
+        False otherwise
 
-        # The sum of a number with its 2's complement is 0
-        return ((c + checksum) & Checksum.__MAX_INT) == 0
+    """
+    _c = __compute(_data)
+
+    # The sum of a number with its 2's complement is 0
+    return ((_c + _checksum) & __MAX_INT) == 0
